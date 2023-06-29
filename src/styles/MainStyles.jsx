1874cd73d7369cc60bb03e109eef2a65ae1c1ecf
sanghee01 <sang6299@naver.com>
2023-06-29 21:44:58 +0900

    feat: 도움말 모달

--- src/styles/MainStyles.jsx
+++ src/styles/MainStyles.jsx
@@ -22,3 +22,42 @@ export const HelpBox = styled.div`
     margin-left: 5px;
   }
 `;
+
+export const Modal = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
+export const ModalOverlay = styled.div`
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+`;
+
+export const ModalContent = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  position: relative;
+  border-radius: 10px;
+  width: 70%;
+`;
+
+export const ModalImg = styled.div`
+  & > img {
+    width: 90%;
+    height: 90%;
+    margin: auto;
+  }
+`;
+
+export const ModalNextBtn = styled.img`
+  width: 40px;
+  height: 80px;
+`;
